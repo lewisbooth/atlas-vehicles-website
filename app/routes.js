4574931eacad43e8013cef7648a976d7e8f0ea3b
Lewis Booth <lewisbooth@cic218.cic.local>
2016-11-17 09:36:50 +0000

    Functional contact form & hashed ID

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(app, passport) {
+module.exports = function(app, passport, nodemailer, bcrypt, crypto) {
 
     // =====================================
     // HOME PAGE (with login links) ========
@@ -28,7 +28,7 @@ module.exports = function(app, passport) {
 
     // =====================================
     // CONTACT =============================
-    // =====================================
+    // =====================================   
     // show the contact form
     app.get('/contact', function(req, res) {
         // render the page and pass in any flash data if it exists
@@ -36,11 +36,7 @@ module.exports = function(app, passport) {
     });
 
     // process the contact form
-    app.post('/contact', passport.authenticate('contact-form', {
-        successRedirect : '/success', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
+    app.post('/contact', contactFormEmailer);
 
     // =====================================
     // CREATE ACCOUNT ======================
@@ -58,7 +54,7 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-    app.get('/success', isLoggedIn, function(req, res) {
+    app.get('/success', function(req, res) {
         res.render('success.ejs', {
             user : req.user // get the user out of session and pass to template
         });
@@ -78,11 +74,90 @@ module.exports = function(app, passport) {
 
     // =====================================
     // LOGOUT ==============================
-    // =====================================
+    // ===================================== 
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
-    });
+    });    
+
+    // =====================================
+    // CONTACT FORM EMAILER ================
+    // ===================================== 
+
+    // Creates new user in database with 'authenticated = false' field, then sends an email to admin for them to manually authenticate the user using a URL (see GET /authenticate handler). Once user is authenticated they can log in at /login.
+
+    function contactFormEmailer(req, res) {        
+
+
+        //Sanitize inputs (tag removal)
+        var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+        var tagOrComment = new RegExp(
+            '<(?:'
+            // Comment body.
+            + '!--(?:(?:-*[^->])*--+|-?)'
+            // Special "raw text" elements whose content should be elided.
+            + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+            + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+            // Regular name
+            + '|/?[a-z]'
+            + tagBody
+            + ')>',
+            'gi');
+        function sanitise(html) {
+            var oldHtml;
+            do {
+                oldHtml = html;
+                html = html.replace(tagOrComment, '');
+            } while (html !== oldHtml);
+            return html.replace(/</g, '&lt;');
+        }
+
+        //Generate hashed user ID
+        var SHA1 = function(input){
+            return crypto.createHash('sha1').update(JSON.stringify(input)).digest('hex')
+        }
+        
+        
+        var userID = SHA1(userEmail);
+        
+        // Email template        
+        var content =                 
+            '<p><b>Name:</b><br>\n\n' + sanitise(req.body.name) + '</p>' +
+            '<p><b>Email:</b><br>\n\n' + sanitise(req.body.email) + '</p>' +
+            '<p><b>Tel:</b><br>\n\n' + sanitise(req.body.phone) + '</p>' +
+            '<p><b>Organisation:</b><br>\n\n' + sanitise(req.body.organisation) + '</p>' +
+            '<p><b>Vehicle Requirements:</b><br>\n\n' + sanitise(req.body.message) + '</p>' +
+            '<p><b>User ID:</b><br>\n\n' + userID + '</p>' +
+            '<p><b><a href="http://localhost:8080/authenticate?' + userID + '">Authenticate this user</a></b></p>';
+
+        mailOptions = {
+            from: 'Atlas Vehicles<atlasvehiclesquery@gmail.com>', // sender address
+            to: 'lewis.booth@ciconline.co.uk', // list of receivers
+            subject: 'Atlas Vehicles – Access request from \n\n' + req.body.name, // Subject line            
+            html: content
+        };
+
+        //Send the mail
+        var transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: 'atlasvehiclesquery@gmail.com', // Your email id
+                pass: 'AtlasVehicles1' // Your password
+            } 
+        });
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log('ERROR in authorisation request from: ' + req.body.email + error);
+                return done(req.flash('contactMessage', 'Error sending email'));
+            }
+            
+            console.log('Authorisation request from: ' + req.body.email + info.response);
+            res.redirect('/success');
+        });
+    }
 };
 
 // route middleware to make sure a user is logged in
@@ -95,3 +170,4 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.render('index.ejs');
 }
+
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var path = require('path')
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
-var configDB = require('./config/database.js');
+var express         = require('express');
+var app             = express();
+var port            = process.env.PORT || 8080;
+var path            = require('path');
+var mongoose        = require('mongoose');
+var passport        = require('passport');
+var bcrypt          = require('bcrypt-nodejs');
+var crypto          = require('crypto');
+var flash           = require('connect-flash');
+var morgan          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var session         = require('express-session');
+var nodemailer      = require('nodemailer');
+var configDB        = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
+
+// Enable SSL in production
+
+//  var forceSsl = function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     return next();
+//  };
+
+// app.use(forceSsl);
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -34,11 +46,11 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport, nodemailer, bcrypt, crypto); // load our routes and pass in our app and fully configured passport
 
 // static files ================================================================
 app.use(express.static(path.join(__dirname, 'public'))); // routes static file requests to /public directory
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('The magic happens on port ' + port); 
